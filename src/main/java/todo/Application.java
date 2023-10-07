925a39f942d02c5039ea3854620e09c1dd8bbd22
mikael-ayala <mikael.ayala.santos@gmail.com>
2023-10-07 19:05:10 -0300

    fix CORS and add OpenApiConfig

--- src/main/java/todo/Application.java
+++ src/main/java/todo/Application.java
@@ -1,11 +1,11 @@
 package todo;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+@OpenAPIDefinition(info = @Info(title = "To Do App API", version = "1", description = "To Do App REST API using Spring"))
 @SpringBootApplication
 public class Application {
 	public String PORT = System.getenv("PORT");
