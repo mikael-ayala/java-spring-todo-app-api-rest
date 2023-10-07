4b758b26d62a26f7d66a9c821ef9e0aabc727919
mikael-ayala <mikael.ayala.santos@gmail.com>
2023-10-07 18:04:08 -0300

    build test

--- src/main/java/todo/Application.java
+++ src/main/java/todo/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @OpenAPIDefinition(info = @Info(title = "To Do App API", version = "1", description = "To Do App REST API using Spring"))
 @SpringBootApplication
 public class Application {
-
+	public String PORT = System.getenv("PORT");
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
